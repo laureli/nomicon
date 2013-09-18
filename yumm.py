afde1ba595b7bba5384069c42e33693957ab018e
laureli <laureli.mallek@gmail.com>
2013-09-17 21:59:20 -0700

    yummly api integration start

---
+++ yumm.py
@@ -0,0 +1,14 @@
+# experimenting with yummly api
+
+
+# API endpoint is:  http://api.yummly.com/v1
+
+
+app-key = "9e8925d2e0663780addbb173657dda52"
+app-id = 32f5db9e
+
+# sample functional query:
+# 	http://api.yummly.com/v1/api/recipes?_app_id=32f5db9e&_app_key=9e8925d2e0663780addbb173657dda52&q=strawberry+beer
+
+
+all i need to do to change the search is to insert parameters into the url
