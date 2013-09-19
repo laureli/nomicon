55974c907b41b6f30a77bca84a8016651cdd6eca
laureli <laureli.mallek@gmail.com>
2013-09-18 22:29:49 -0700

    another tweak

--- yumm.py
+++ yumm.py
@@ -5,9 +5,8 @@ from codes import api_key, api_id
 terms_list = ""
 params =''
 
-
 # sample functional query:
-# 	http://api.yummly.com/v1/api/recipes?_app_id=32f5db9e&_app_key=9e8925d2e0663780addbb173657dda52&q=strawberry+beer+
+# 	http://api.yummly.com/v1/api/recipes?_app_id=___ ###SAMPLE ID___ &_app_key=___ SAMPLE KEY __ &q=strawberry+beer+
 
 # search_url = http://api.yummly.com/v1/api/recipes?_app_id=api_id&_app_key=api_key"
 
