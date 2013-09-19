b61c53eef19c68bd3c5b6f933989fb6d39b89416
laureli <laureli.mallek@gmail.com>
2013-09-18 22:24:18 -0700

    request formatting initial commit

--- yumm.py
+++ yumm.py
@@ -2,27 +2,32 @@ from urllib2 import urlopen
 from json import load 
 from codes import api_key, api_id
 
-# API endpoint is:  http://api.yummly.com/v1
+terms_list = ""
+parameters = []
+params =''
+
 
 # sample functional query:
 # 	http://api.yummly.com/v1/api/recipes?_app_id=32f5db9e&_app_key=9e8925d2e0663780addbb173657dda52&q=strawberry+beer+
 
 # search_url = http://api.yummly.com/v1/api/recipes?_app_id=api_id&_app_key=api_key"
 
-search_terms=raw_input('what ingredients do you want to search for -> ')
-# remove non-alpha characters -->
-# split words apart
-# add + to the end of the word
-# concatenate into a string and save it as parameters
+search_input=raw_input('what ingredients do you want to search for -> ')
 
-parameters = "" # will actually be the output of search terms
+if ',' in search_input:
+	terms_list = search_input.split(',')
+else:
+	terms_list = search_input.split()
 
-url = 'http://api.yummly.com/v1/api/recipe/recipe-id?_app_id='
+print 'TERMS LIST IS:', terms_list
+
+for item in terms_list:
+	params = params+item+'+'
+
+url =' http://api.yummly.com/v1/api/recipes?_app_id='
 url += api_id+'&_app_key='+api_key
-url +='&q='+parameters
-
-print api_key
+url +='&q='+params
 
 print url
 
-print search_terms
+
