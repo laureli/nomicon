44001a04b54a9b32de46a09e010e8184d95bb15b
laureli <laureli.mallek@gmail.com>
2013-09-19 18:08:30 -0700

    flask-mail now with SMTP errors on contact.html

--- routes.py
+++ routes.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
-from flask.ext.mail import Message, Mail
+from flask_mail import Message, Mail
 from codes import email_username, email_pw
 import mixpanel
 
 # APPLICATION CONFIG
-mail = Mail()
  
 app = Flask(__name__)
+mail = Mail(app)
+
  
 app.secret_key = 'development key'
  
@@ -16,8 +17,11 @@ app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = email_username
 app.config["MAIL_PASSWORD"] = email_pw
- 
-mail.init_app(app)
+
+app.config["MAIL_DEFAULT_SENDER"] = email_username
+
+mail = Mail(app)
+
 
 # APPLICATION ORGANIZATION
 
@@ -39,17 +43,30 @@ def contact():
 			flash('please fill in all fields correctly')
 			return render_template('contact.html', form=form)
 		else:
-			msg=Message(form.subject.data, sender='sender-contact@example.com', recipients=['recip@example.com'])
-			msg.body = """
-			From: %s <%s>
-			%s
-			""" %(form.name.data, form.email.data, form.message.data)
-			mail.send(msg)
-
-			return render_template('contact.html', success=True)
+		    msg = Message(form.subject.data, sender='sample@sample.com', recipients=email_username)
+		    msg.body = """
+		    From: %s <%s>
+		    %s
+		    """ % (form.name.data, form.email.data, form.message.data)
+		    mail.send(msg)
+		    return render_template('contact.html', success=True)
 
 	elif request.method == 'GET':
 		return render_template('contact.html', form=form)
+
+
+
+@app.route("/mailtest")
+def mailtest():
+
+    msg = Message("Hello",
+                  sender=email_username,
+                  recipients=[email_username]
+                  )
+    mail.send(msg)
+    return 'maybe your email got sent'
+
+
 
 if __name__=='__main__':
 	app.run(debug=True)
